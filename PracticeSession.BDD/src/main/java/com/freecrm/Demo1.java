1abaf4e0b41874ec1d73e77431cc873c0bf92133
seleniumk189 <seleniumk189@gmail.com>
2018-09-10 23:26:35 +0530

    First Commit

---
+++ PracticeSession.BDD/src/main/java/com/freecrm/Demo1.java
@@ -0,0 +1,53 @@
+package com.freecrm;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class Demo1 {
+	
+	@Given("^I want to write a step with precondition$")
+	public void i_want_to_write_a_step_with_precondition()  {
+	    
+	    
+	}
+
+	@Given("^some other precondition$")
+	public void some_other_precondition()  {
+	    
+	    
+	}
+
+	@When("^I complete action$")
+	public void i_complete_action()  {
+	    
+	    
+	}
+
+	@When("^some other action$")
+	public void some_other_action()  {
+	    
+	    
+	}
+
+	@When("^yet another action$")
+	public void yet_another_action()  {
+	    
+	    
+	}
+
+	@Then("^I validate the outcomes$")
+	public void i_validate_the_outcomes()  {
+	    
+	    
+	}
+
+	@Then("^check more outcomes$")
+	public void check_more_outcomes()  {
+	    
+	    
+	}
+
+
+
+}
